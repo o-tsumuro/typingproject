0fcb5040e9b5906891177837106477a72e7d87b6
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-03 17:10:20 +0900

    CustomUserCreationForm定義

---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    model = CustomUser
+    fields = ('username', 'password')
+    
