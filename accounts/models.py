a79bbddc071f0e475d88e8f6b65d6230406484ba
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-03 16:39:45 +0900

    CustomUserモデル追加、migrate

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
-class CusotmUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     pass
