9cef4fa20b5efb986f6be16c50e721b02f32e774
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-03 19:02:31 +0900

    各種URL設定

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
-from . import views
 from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
 urlpatterns = [
+    path('', views.SignUpView.as_view(), name='signup'),
     
+    path('login/',
+         auth_views.LoginView.as_view(template_name='login'),
+         name='login'),
 ]
--- typingapp/urls.py
+++ typingapp/urls.py
@@ -1,8 +1,8 @@
-from . import views
 from django.urls import path
+from . import views
 
 app_name = "typingapp"
 
 urlpatterns = [
-    
+    path('index/', views.IndexView.as_view(), name='index'),
 ]
