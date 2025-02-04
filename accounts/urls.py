aecc9a2f2d6a6657c763631f3022529a39727f9e
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-04 21:08:10 +0900

    ログアウト機能実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -10,4 +10,9 @@ urlpatterns = [
     path('login/',
          auth_views.LoginView.as_view(template_name='login.html'),
          name='login'),
+
+    path('logout/',
+         auth_views.LogoutView.as_view(next_page=reverse_lazy('typingapp:index')),
+         name='logout'),
+
 ]
