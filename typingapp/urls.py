a0eca66460e0a9b0cb973ed8f019065dd4166748
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-05 23:39:02 +0900

    content_list用url追記

--- typingapp/urls.py
+++ typingapp/urls.py
@@ -6,4 +6,5 @@ app_name = "typingapp"
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('<int:pk>/', views.IndexView.as_view(), name='typing'),
+    path('content_list/', views.ContentListView.as_view(), name="content_list"),
 ]
