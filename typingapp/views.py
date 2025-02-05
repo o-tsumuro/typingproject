814ed32a3f7cfbcc98a89c856e5965868233f3dd
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-05 23:53:30 +0900

    views,context_object_name修正

--- typingapp/views.py
+++ typingapp/views.py
@@ -4,7 +4,7 @@ from .models import Content
 class IndexView(generic.DetailView):
     model = Content
     template_name = "index.html"
-    context_object_name = "typing_object"
+    context_object_name = "content_object"
 
     def get_object(self, queryset=None):
         pk = self.kwargs.get("pk", 1)
