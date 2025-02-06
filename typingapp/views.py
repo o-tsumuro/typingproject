4e5258ce907f1eeddf506b118995b9ec4ab318ae
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-06 20:07:54 +0900

    views.py修正(データの送信をIndexViewにまとめる)

--- typingapp/views.py
+++ typingapp/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from .models import Content, History
+from .forms import HistoryForm
 from django.shortcuts import redirect
 
 class IndexView(generic.DetailView):
@@ -11,17 +12,16 @@ class IndexView(generic.DetailView):
         pk = self.kwargs.get("pk", 1)
         return Content.objects.get(pk=pk)
     
+    def post(self, request, *args, **kwargs):
+        typing_time = request.POST.get('typing_time')
+        pk = self.kwargs['pk'] #URLからpkを取得
+        content = Content.objects.get(pk=pk) #取得したpkのContentモデルのインスタンスを取得
+        history = History(user=request.user, title=content, typing_time=typing_time)
+        history.save()
+        return redirect("typingapp:index")
+
 class ContentListView(generic.ListView):
     model = Content
     template_name = "content_list.html"
     context_object_name = "content_object"
     queryset = Content.objects.order_by("pk")
-
-class CreateHistoryView(generic.CreateView):
-    form_class = History
-
-    def form_valid(self, form):
-        history = form.save(commit=False)
-        history.user = self.request.user
-        history.save()
-        return redirect("typingapp:index")
