df820d3c505bcd91921a54e16b9fd9e757d021e3
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-07 12:19:27 +0900

    urls追加、修正

--- typingproject/urls.py
+++ typingproject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path('', include("typingapp.urls")),
+    path('content/', include("contentapp.urls")),
 ]
