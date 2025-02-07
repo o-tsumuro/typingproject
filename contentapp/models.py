5e2756ce72fac987bee5aace08dc427a9fad9dc3
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-07 12:42:29 +0900

    Contentテーブル移動,再マイグレート

--- contentapp/models.py
+++ contentapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Content(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    title = models.CharField(max_length=30, unique=True)
+    sentence = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- typingapp/models.py
+++ typingapp/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser
-from django.conf import settings
-
-class Content(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
-    title = models.CharField(max_length=30, unique=True)
-    sentence = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
+from contentapp.models import Content
     
 class History(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
