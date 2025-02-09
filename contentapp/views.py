e5802bb5cffa92c4608102ef594ccec7beea7d52
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-09 21:43:26 +0900

    views設計-CreateContentView

--- contentapp/views.py
+++ contentapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
+from .forms import ContentForm
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# Create your views here.
+class CreateContentView(generic.CreateView):
+    form_class = ContentForm
+    template_name = "create_content.html"
+
+    def form_valid(self, form):
+        data = form.save(commit=False)
+        data.user = self.request.user
+        data.save()
+        success_url = reverse('typingapp:typing', kwargs={'pk':data.pk})
+        return redirect(success_url)
