189dd0fb8fa82a28004ce1095036654f786f6743
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-12 15:38:25 +0900

    typing_listルーティング設定

--- contentapp/urls.py
+++ contentapp/urls.py
@@ -6,4 +6,5 @@ app_name = "contentapp"
 urlpatterns = [
     path('create_content/', views.CreateContentView.as_view(), name="create_content"),
     path('mypage/<str:username>/', views.MyPageView.as_view(), name="mypage"),
+    path('typing_list/', views.TypingListView.as_view(), name="typing_list"),
 ]
