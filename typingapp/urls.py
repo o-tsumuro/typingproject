684847c9f1d33bface127033570805e510838199
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-15 23:36:02 +0900

    result.htmlルーティング

--- typingapp/urls.py
+++ typingapp/urls.py
@@ -6,5 +6,5 @@ app_name = "typingapp"
 urlpatterns = [
     path('', views.first_view, name="index"),
     path('<int:pk>/', views.IndexView.as_view(), name='typing'),
-
+    path('result/<int:pk>/<int:current_time>/', views.ResultView.as_view(), name='result'),
 ]
