30418eeaa7c25906f60f616043d6c82f256977aa
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-15 12:53:38 +0900

    forms.py-カテゴリー選択追加[

--- contentapp/forms.py
+++ contentapp/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Content
 
 class ContentForm(ModelForm):
     class Meta:
         model = Content
-        fields = ['title', 'sentence', 'is_public']
+        fields = ['title', 'sentence', 'category', 'is_public']
+        widgets = {
+            'category': forms.Select(),
+        }
