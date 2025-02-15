faaa32e662c905309d04eae84e6b1c0ab93f0c02
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-15 13:21:46 +0900

    CategoryAdmin追加

--- contentapp/admin.py
+++ contentapp/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Content
+from .models import Content, Category
 
 class ContentAdmin(admin.ModelAdmin):
-    list_display = ("user", "title", "is_public", "play_count")
+    list_display = ("user", "title", "category", "is_public", "play_count", "created_at")
     list_display_links = ("user", "title")
 
 admin.site.register(Content, ContentAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("catogory")
+
+admin.site.register(Category, ContentAdmin)
