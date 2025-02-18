911cafea908d2de22960b08ea9194727d441fd12
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-18 12:41:08 +0900

    guest_resultルーティング設定

--- typingapp/urls.py
+++ typingapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.first_view, name="index"),
     path('<int:pk>/', views.IndexView.as_view(), name='typing'),
     path('result/<int:pk>/<int:current_time>/', views.ResultView.as_view(), name='result'),
+    path('guest_result/<int:pk>/<int:typing_time>', views.GuestResultView.as_view(), name='guest_result'),
 ]
