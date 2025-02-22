f7728250fc6bf0ea9329edbb5e92351ead12f954
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-22 12:06:40 +0900

    urls修正

--- typingapp/urls.py
+++ typingapp/urls.py
@@ -6,6 +6,6 @@ app_name = "typingapp"
 urlpatterns = [
     path('', views.first_view, name="index"),
     path('<int:pk>/', views.IndexView.as_view(), name='typing'),
-    path('result/<int:pk>/<int:current_time>/', views.ResultView.as_view(), name='result'),
+    path('result/<int:pk>/<int:typing_time>/', views.ResultView.as_view(), name='result'),
     path('guest_result/<int:pk>/<int:typing_time>', views.GuestResultView.as_view(), name='guest_result'),
 ]
