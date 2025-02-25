ca534b4f2c991f83f138a1670bcdc37ee98aa152
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-25 22:56:40 +0900

    ランダム機能追加

--- typingproject/settings.py
+++ typingproject/settings.py
@@ -137,4 +137,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
-LOGIN_REDIRECT_URL = 'typingapp:index'
+LOGIN_REDIRECT_URL = 'typingapp:randomTyping'
