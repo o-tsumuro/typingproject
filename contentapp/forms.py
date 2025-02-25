1ccd4df683c649ed14dff5f3148c4d35bda99b4f
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-25 12:09:54 +0900

    forms.py - タイトル重複エラー処理追加

--- contentapp/forms.py
+++ contentapp/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Content, Category
+from django.core.exceptions import ValidationError
 
 class ContentForm(ModelForm):
     class Meta:
@@ -9,3 +10,9 @@ class ContentForm(ModelForm):
         widgets = {
             'category': forms.Select(choices=Category.objects.all())
         }
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if Content.objects.filter(title=title).exists():
+            raise ValidationError('このタイトルは既に使用されています。')
+        return title
