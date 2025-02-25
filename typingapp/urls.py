19188f92f859dd95c8e714fa00f2239761c5b4e8
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-02-25 22:16:09 +0900

    ルーティング設定

--- typingapp/urls.py
+++ typingapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "typingapp"
 
 urlpatterns = [
-    path('', views.first_view, name="index"),
+    path('', views.TopPageView.as_view(), name="index"),
     path('<int:pk>/', views.IndexView.as_view(), name='typing'),
     path('result/<int:pk>/<int:typing_time>/', views.ResultView.as_view(), name='result'),
     path('guest_result/<int:pk>/<int:typing_time>', views.GuestResultView.as_view(), name='guest_result'),
